9e89a2a394d8d62fb5754576bd5e511b89d064c8
Sid <siddhartha.arya@gmail.com>
2015-07-10 12:54:59 -0500

    Initial Commit

---
+++ School_Anouncment_FinalApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  School_Anouncment_FinalApp
+//
+//  Created by Siddhartha Arya on 10/7/15.
+//  Copyright © 2015 Siddhartha Arya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
