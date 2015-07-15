79c3828e207a0b4d916b1b0614033fb725a348f1
Sid <siddhartha.arya@gmail.com>
2015-07-15 10:17:51 -0500

    new Contact Us view controller

--- School_Anouncment_FinalApp/ContactUsViewController.swift
+++ School_Anouncment_FinalApp/ContactUsViewController.swift
@@ -2,34 +2,17 @@
 //  ContactUsViewController.swift
 //  School_Anouncment_FinalApp
 //
-//  Created by Siddhartha Arya on 14/7/15.
+//  Created by Siddhartha Arya on 15/7/15.
 //  Copyright © 2015 Siddhartha Arya. All rights reserved.
 //
 
 import UIKit
-import Foundation
-import MessageUI
 
 class ContactUsViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-    @IBOutlet weak var nameText: UITextField!
-    
-    @IBOutlet weak var phoneText: UITextField!
-    
-    @IBOutlet weak var emailText: UITextField!
-    
-    
-    @IBOutlet weak var questionText: UITextField!
-    
-    
-    @IBAction func sendButtonTapped(sender: UIButton) {
-        
-        
-    }
-    
+    @IBOutlet weak var subject: UITextField!
+    @IBOutlet weak var body: UITextView!
 }
-
