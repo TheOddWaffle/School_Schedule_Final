2236d0763bddf631a01c6dac119dc8f9f7855919
Sid <siddhartha.arya@gmail.com>
2015-07-15 10:35:01 -0500

    contact us text fields and view coding

--- School_Anouncment_FinalApp/ContactUsViewController.swift
+++ School_Anouncment_FinalApp/ContactUsViewController.swift
@@ -7,16 +7,48 @@
 //
 
 import UIKit
+import MessageUI
 
 class ContactUsViewController: UIViewController, MFMailComposeViewControllerDelegate, UITextFieldDelegate, UITextViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+   
     @IBOutlet weak var subject: UITextField!
     
     
     @IBOutlet weak var body: UITextView!
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        subject.delegate = self
+        body.delegate = self
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        body.text = textView.text
+    
+    if text == "\n" {
+    textView.resignFirstResponder()
+    
+    return false
+        
+    }
+    
+    return true
+        
+    }
 }
+
